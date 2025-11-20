f2308062b7930d7222dbbe55882f03f637e19134
cloudoraserv-SRK <cloudoraserv@gmail.com>
2025-11-21 04:52:12 +0530

    Update apply.js

--- apply/apply.js
+++ apply/apply.js
@@ -64,3 +64,11 @@ document.addEventListener('DOMContentLoaded', () => {
         yearSpan.textContent = new Date().getFullYear();
     }
 });
+
+const hamburger = document.getElementById('hamburger');
+  const mobileNav = document.getElementById('mobileNav');
+
+  hamburger.addEventListener('click', () => {
+    mobileNav.classList.toggle('active');
+  });
+
