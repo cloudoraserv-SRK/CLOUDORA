4a2914db2d73ab62bc93713fa5f9e92659785b24
cloudoraserv-SRK <cloudoraserv@gmail.com>
2025-11-26 16:54:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -179,7 +179,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // Supabase
 const supabaseUrl = "https://rfilnqigcadeawytwqmz.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmaWxucWlnY2FkZWF3eXR3cW16Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxMzE2NTIsImV4cCI6MjA3OTcwNzY1Mn0.1wtcjczrzhv2YsE7hGQL11imPxmFVS4sjxlJGvIZ26o";
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
 document.getElementById("contactForm").addEventListener("submit", async function(e) {
   e.preventDefault(); // stop page reload
