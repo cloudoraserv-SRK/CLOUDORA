9ee1e0264de32f83f09f54833d3263781a13e021
cloudoraserv-SRK <cloudoraserv@gmail.com>
2025-11-30 07:00:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -234,9 +234,6 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     }
 
-  });
-});
-
 // --- google translate ---
 function googleTranslateElementInit() {
   new google.translate.TranslateElement({
