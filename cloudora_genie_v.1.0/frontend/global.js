99e76daa646228ae4d307a0a7102331f07d14dd1
cloudoraserv-SRK <cloudoraserv@gmail.com>
2025-12-11 06:50:04 +0530

    Update global.js

--- cloudora_genie_v.1.0/frontend/global.js
+++ cloudora_genie_v.1.0/frontend/global.js
@@ -5,7 +5,7 @@
 const CONFIG = {
   SUPA_URL: "https://gnihhujwqxkigwwnksgw.supabase.co",
   SUPA_ANON: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImduaWhodWp3cXhraWd3d25rc2d3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4OTc2MjcsImV4cCI6MjA4MDQ3MzYyN30.tOYXHySzEQ2py-NUNBmlpp7QWciaGSSD_NeWpLbeRaw",
-  BACKEND: "http://localhost:8787",
+  BACKEND: "cloudora.railway.internal",
 
   LANG: localStorage.getItem("lang") || "en",
   CURRENCY: localStorage.getItem("currency") || "INR",
@@ -149,3 +149,4 @@ window.speak = speak;
 window.startListening = startListening;
 window.applyTranslations = applyTranslations;
 window.initializeLanguageSelector = initializeLanguageSelector;
+
