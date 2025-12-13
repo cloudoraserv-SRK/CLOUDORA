157c3c19c136e114b7443e12fd132a426dfa909d
cloudoraserv-SRK <cloudoraserv@gmail.com>
2025-12-13 12:16:51 +0530

    Update supabase.js

--- backend/api/supabase.js
+++ backend/api/supabase.js
@@ -4,9 +4,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const supabase = createClient(
+export const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
-
-export default supabase;
